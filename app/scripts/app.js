4aa6623d4d2b889d5007742a6df53c6866afc13b
morninng <mr_morninng_star_dash@yahoo.co.jp>
2016-01-26 23:38:41 +0900

    initial commit just after yo angular

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name uiRouterPracticeApp
+ * @description
+ * # uiRouterPracticeApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('uiRouterPracticeApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
